dc2a11733fc519abf507888704766d811d30d214
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-10-09 22:26:01 +0800

    create setting

---
+++ lib/settings.dart
@@ -0,0 +1,17 @@
+const double canvasWidth = 400;
+const double canvasHeight = 500;
+const double clothWidth = 60;
+const double clothHeight = 30;
+
+const double gravity = 1200;
+const int physicsAccuracy = 3;
+const double friction = 0.98;
+
+const double spacing = 5;
+const double tearDistance = 30;
+
+const double startX = canvasWidth / 2 - (clothWidth * spacing) / 2;
+const double startY = 20;
+
+const double mouseInfluence = 20;
+const double mouseCut = 7;
