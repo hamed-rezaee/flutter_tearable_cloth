9df8d16201efe5a90a7a37ed769f42e678be7175
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-10-11 19:08:51 +0800

    update settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,17 +1,19 @@
-const double canvasWidth = 400;
-const double canvasHeight = 500;
-const double clothWidth = 60;
-const double clothHeight = 30;
+import 'package:flutter/material.dart';
+
+const double canvasWidth = 800;
+const double canvasHeight = 600;
+const double clothWidth = 80;
+const double clothHeight = 60;
 
 const double gravity = 1200;
 const int physicsAccuracy = 3;
 const double friction = 0.98;
 
 const double spacing = 5;
-const double tearDistance = 30;
+const double tearDistance = 50;
 
-const double startX = canvasWidth / 2 - (clothWidth * spacing) / 2;
-const double startY = 20;
+const Offset start =
+    Offset(canvasWidth * 0.5 - (clothWidth * spacing) * 0.5, 20);
 
-const double mouseInfluence = 20;
+const double mouseInfluence = 40;
 const double mouseCut = 7;
